9e336b9ef64f52c7f9142279c2f82148ec2cbb77
zhunhe <xuzhunhe@gmail.com>
2022-02-13 16:55:09 +0900

    etc: Remove unuse file

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/30 11:21:56 by juhur             #+#    #+#             */
-/*   Updated: 2022/02/03 21:15:36 by juhur            ###   ########.fr       */
+/*   Updated: 2022/02/13 16:45:46 by juhur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 # include <stddef.h>
 
 size_t	ft_strlen(const char *s);
-char	*ft_strchr(const char *s, int c);
 int		ft_strcmp(const char *s1, const char *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 #endif
